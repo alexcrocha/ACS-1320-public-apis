c64c7a82803141a0b82984a3ad337edd2b898ddc
Alex Rocha <rocha.amc@gmail.com>
2023-05-08 23:21:16 -0700

    Update navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const location = useLocation();
 
   const handleScroll = () => {
     const currentScrollY = window.scrollY;
@@ -20,6 +21,8 @@ function Navbar() {
     };
   }, []);
 
+  const activeLinkClass = "border-b-2 border-white";
+
   return (
     <nav
       className={`navbar fixed top-0 left-0 w-full bg-gray-800 p-4 md:flex md:justify-between md:items-center ${isScrolled ? "md:bg-gray-900" : ""}`}
@@ -30,14 +33,14 @@ function Navbar() {
       >
         <Link to={"/"}>PUBLIC APIs</Link>
       </h3>
-      <section className="navbar-links flex justify-center md:justify-end space-x-4 md:space-x-8">
-        <Link to={"/"} aria-label="Go to Home">
+      <section className="navbar-links flex justify-center md:justify-end space-x-4 md:space-x-8 font-bold">
+        <Link to={"/"} aria-label="Go to Home" className={`${location.pathname === "/" ? activeLinkClass : ""}`}>
           Home
         </Link>
-        <Link to={"/godlist"} aria-label="Go to API Godlist">
+        <Link to={"/godlist"} aria-label="Go to API Godlist" className={`${location.pathname === "/godlist" ? activeLinkClass : ""}`}>
           Godlist
         </Link>
-        <Link to={"/categories"} aria-label="Go to Categories">
+        <Link to={"/categories"} aria-label="Go to Categories" className={`${location.pathname === "/categories" ? activeLinkClass : ""}`}>
           Categories
         </Link>
       </section>
